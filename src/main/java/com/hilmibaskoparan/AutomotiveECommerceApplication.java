09eafde69420de109ebd2ee0df4b8d156ba20634
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-12 09:00:34 +0300

    Initial commit

---
+++ src/main/java/com/hilmibaskoparan/AutomotiveECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hilmibaskoparan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveECommerceApplication.class, args);
+	}
+
+}
