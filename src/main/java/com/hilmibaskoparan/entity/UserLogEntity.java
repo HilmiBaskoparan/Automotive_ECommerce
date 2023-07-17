03578adc4a1c9a471f8cde67cbd6a2865cecfe72
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-17 12:15:16 +0300

    UserLog Entity. Close #13

---
+++ src/main/java/com/hilmibaskoparan/entity/UserLogEntity.java
@@ -0,0 +1,24 @@
+package com.hilmibaskoparan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_logs")
+public class UserLogEntity extends BaseEntity {
+
+    //private User user;
+    private String logDescription;
+    private Date loginDate;
+    private Date logoutDate;
+}
