f7470c85d0961ab03570ada25487e2683c7293d2
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-17 12:11:18 +0300

    CreditCard Entity. Close #11

---
+++ src/main/java/com/hilmibaskoparan/entity/CreditCardEntity.java
@@ -0,0 +1,26 @@
+package com.hilmibaskoparan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "credit_cards")
+public class CreditCardEntity extends BaseEntity {
+
+    private String bankName;
+    private String creditCardNumber;
+    //private User User;
+    // private User user.getName() + user.getUsername()
+    private Date expirationDate;
+    //private int cvc;
+}
