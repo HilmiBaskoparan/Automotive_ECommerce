e68158ec32d15d363a32c0257ee4f4ec22e82b67
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-17 11:57:08 +0300

    Base Entity. Close #14

---
+++ src/main/java/com/hilmibaskoparan/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.hilmibaskoparan.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date systemDate;
+}
