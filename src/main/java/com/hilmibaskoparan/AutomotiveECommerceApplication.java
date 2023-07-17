c015fbc4f82d24200026a552438bbc12e7467406
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-17 11:37:24 +0300

    exclude SecurityAutoConfiguration.class

--- src/main/java/com/hilmibaskoparan/AutomotiveECommerceApplication.java
+++ src/main/java/com/hilmibaskoparan/AutomotiveECommerceApplication.java
@@ -1,10 +1,20 @@
 package com.hilmibaskoparan;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class AutomotiveECommerceApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutomotiveECommerceApplication.class, args);
