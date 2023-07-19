ab5d47fc2a1eed6f1ec78fd3887e0e0b5a7177bb
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-19 10:50:53 +0300

    Revise of User Entity

--- src/main/java/com/hilmibaskoparan/entity/User.java
+++ src/main/java/com/hilmibaskoparan/entity/User.java
@@ -1,13 +1,14 @@
 package com.hilmibaskoparan.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -20,16 +21,26 @@ public class User extends BaseEntity implements Serializable {
 
     public static final Long serialVersionUID = 1L;
 
-    private String name;
-    private String surname;
+    @Column(length = 30 , nullable = false)
+    private String firstName;
+
+    @Column(length = 30 , nullable = false)
+    private String lastName;
+
+    @Column(length = 30 , nullable = false)
     private String username;
+
+    @Column(length = 80, nullable = false, unique = true)
     private String email;
     private String password;
+
+    @Column(unique = true)
+    @Size(min = 10, max = 10)
     private String phone;
     private Boolean sex;
-    private Date birthDate;
+    private LocalDate birthDate;
 
-    @OneToOne(mappedBy = "user")
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Basket basket;
 
     @OneToMany(mappedBy = "user")
@@ -40,4 +51,7 @@ public class User extends BaseEntity implements Serializable {
 
     @OneToMany(mappedBy = "user")
     private List<UserLog> userLogs;
+
+    // createdTime
+    // updatedTime
 }
