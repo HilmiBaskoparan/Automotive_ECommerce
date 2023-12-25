fcb9fab05899099daf77732243071f2625d6153d
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-12-26 02:16:05 +0300

    User Entity

---
+++ src/main/java/com/hilmibaskoparan/model/entity/User.java
@@ -0,0 +1,64 @@
+package com.hilmibaskoparan.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import javax.management.relation.Role;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "user_name"),
+        @UniqueConstraint(columnNames = "email") })
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User extends BaseEntity {
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean emailVerified = false;
+
+    @Column(name = "provider_id")
+    private String providerId;
+
+    @Column(name = "failed_login_attempts")
+    private int failedLoginAttempts;
+
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked = true;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+
+    @OneToMany(mappedBy = "user")
+    private List<CreditCard> creditCards;
+
+    @OneToMany(mappedBy = "user")
+    private List<Address> addresses;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles = new ArrayList<>();
+
+}
