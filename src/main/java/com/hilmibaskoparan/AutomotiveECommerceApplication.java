86953eff9035d6feb0ba896a1693248091f5f73d
Hilmi Baskoparan <h.baskoparan@gmail.com>
2024-01-03 23:59:46 +0300

    ModelMapper and PasswordEncoder Bean

--- src/main/java/com/hilmibaskoparan/AutomotiveECommerceApplication.java
+++ src/main/java/com/hilmibaskoparan/AutomotiveECommerceApplication.java
@@ -2,9 +2,13 @@ package com.hilmibaskoparan;
 
 import com.hilmibaskoparan.config.AppProperties;
 import jakarta.annotation.PostConstruct;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.TimeZone;
@@ -21,6 +25,16 @@ public class AutomotiveECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutomotiveECommerceApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
